f2029bbfaa605bce270018bdd0fd27080ecc830e
Thabo Mbatha <thabothomasmbatha@gmail.com>
2024-06-11 13:40:33 +0200

    work

--- tash/views.py
+++ tash/views.py
@@ -2,21 +2,26 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 
-# class AboutPage(TemplateView):
-#     template_name = 'about.html'
-
-# class ContactPage(TemplateView):
-#     template_name = 'contacts.html'
-
-# class ProductsPage(TemplateView):
-#     template_name = 'products.html'
-
-# class TestimonialsPage(TemplateView):
-#     template_name = 'testimonials.html'
-
 class WelcomePage(TemplateView):
     template_name = 'welcome_page.html'
 
+class AboutPage(TemplateView):
+    template_name = 'about.html'
+
+class ServicesPage(TemplateView):
+    template_name = 'services.html'
+
+class GallaryPage(TemplateView):
+    template_name = 'gallary.html'
+
+class TeamPage(TemplateView):
+    template_name = 'team.html'
+
+class BlogPage(TemplateView):
+    template_name = 'blog.html'
+
+class ContactPage(TemplateView):
+    template_name = 'contact.html'
 class ExitPage(TemplateView):
     template_name = 'exit_page.html'
 
