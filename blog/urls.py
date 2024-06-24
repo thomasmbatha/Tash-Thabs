a2324799d1b3890deb4478f79e69014e29f22736
Thabo Mbatha <thabothomasmbatha@gmail.com>
2024-06-24 12:01:16 +0200

    work

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 # blog/urls.py
 from django.urls import path, include
-from .views import BlogHome
+from .views import BlogHome,DetailArcticleView
 
 urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
     path('blog-home/', BlogHome.as_view(), name='blog_home'),
+    path('<int:pk>/', DetailArcticleView.as_view(), name='detail_article'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 # blog/views.py
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Article
 # Create your views here.
 
@@ -11,3 +11,7 @@ class BlogHome(ListView):
     queryset = Article.objects.all().order_by('-date')
     template_name = 'blog/blog_home.html'
     paginate_by = 1
+
+class DetailArcticleView(DetailView):
+    model = Article
+    template_name = 'blog/blog_post.html'
