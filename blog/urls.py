be735adb02a8d45d63ba0fb3f1a00d31c9a50ac6
Thabo Mbatha <thabothomasmbatha@gmail.com>
2024-06-24 21:34:43 +0200

    home

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 # blog/urls.py
 from django.urls import path, include
-from .views import BlogHome,DetailArcticleView
+from .views import BlogHome,DetailArcticleView,LikeArticle,Featured
 
 urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
     path('blog-home/', BlogHome.as_view(), name='blog_home'),
     path('<int:pk>/', DetailArcticleView.as_view(), name='detail_article'),
+    path('<int:pk>/like', LikeArticle.as_view(), name='like_article'),
+    path('featured/', Featured.as_view(), name='featured')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 # blog/views.py
-from django.shortcuts import render
+from typing import Any
+from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
 from .models import Article
@@ -12,9 +13,23 @@ class BlogHome(ListView):
     template_name = 'blog/blog_home.html'
     paginate_by = 1
 
+class Featured(ListView):
+    model = Article
+    queryset = Article.objects.filter(featured=True).order_by('-date')
+    template_name = 'blog/featured.html'
+    paginate_by = 1
 class DetailArcticleView(DetailView):
     model = Article
     template_name = 'blog/blog_post.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(DetailArcticleView, self).get_context_data(*args, **kwargs)
+        context['liked_by_user'] = False
+        article = Article.objects.get(id=self.kwargs.get('pk'))
+        if article.likes.filter(pk=self.request.user.id).exists():
+            context['liked_by_user'] = True
+        return context
+
 
 class LikeArticle(View):
     def post(self, request, pk):
@@ -23,5 +38,5 @@ class LikeArticle(View):
             article.likes.remove(request.user.id)
         else:
             article.likes.add(request.user.id)
-
-            # 1:35:45 
+        article.save()
+        return redirect('detail_article', pk)
